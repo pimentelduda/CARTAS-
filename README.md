330cbd162c679ad57433a3b2d652e527853fd7d3
pimentelduda <148362188+pimentelduda@users.noreply.github.com>
2024-10-03 09:33:41 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CARTAS-
+# cartas
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'reino animal ',
+    'Quais são as 3 características do reino animal?',
+    'Os animais são organismos multicelulares, eucariontes e que apresentam nutrição heterotrófica'
+)
+
+criaCartao(
+    'reino animal',
+    'Qual o maior filo do reino animal?',
+    'artrópodes'
+)
+
+criaCartao(
+    'reino animal ',
+    'Como é conhecido o reino animal?',
+    'Animalia ou Metazoa'
+)
+
+criaCartao(
+    'reino anima',
+    'Qual é o menor reino animal?',
+    'Placozoa'
+)
